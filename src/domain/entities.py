d483f1d89d1e9063014881415a18df5764c24e8a
sdediego <sergio.dediego@outlook.com>
2021-07-24 00:00:27 +0200

    Create currency exchange rate entity

--- src/domain/entities.py
+++ src/domain/entities.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from dataclasses import dataclass
+from datetime import date
 
 
 @dataclass
@@ -12,3 +13,19 @@ class Currency:
     def __str__(self) -> str:
         _symbol = f" ({self.symbol}):" if self.symbol else ":"
         return f'{self.code}{_symbol} {self.name}'
+
+
+@dataclass
+class CurrencyExchangeRate:
+    source_currency: Currency
+    exchanged_currency: Currency
+    valuation_date: date
+    rate_value: float
+
+    def __str__(self) -> str:
+        source_currency = self.source_currency.code
+        exchanged_currency = self.exchanged_currency.code
+        return (
+            f'{source_currency}/{exchanged_currency} '
+            f'= {self.rate_value} ({self.valuation_date})'
+        )
