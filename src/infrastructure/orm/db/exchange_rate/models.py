2199923ff694b793f31fa0abe2499c6a902bba14
sdediego <sergio.dediego@outlook.com>
2021-07-24 17:47:20 +0200

    Create currency exchange rate orm model

--- src/infrastructure/orm/db/exchange_rate/models.py
+++ src/infrastructure/orm/db/exchange_rate/models.py
@@ -12,3 +12,17 @@ class Currency(models.Model):
         verbose_name = 'currency'
         verbose_name_plural = 'currencies'
         ordering = ('code',)
+
+
+class CurrencyExchangeRate(models.Model):
+    source_currency = models.ForeignKey(
+        Currency, db_index=True, on_delete=models.CASCADE, related_name='exchanges')
+    exchanged_currency = models.ForeignKey(
+        Currency, db_index=True, on_delete=models.CASCADE)
+    valuation_date = models.DateField(db_index=True)
+    rate_value = models.DecimalField(decimal_places=6, max_digits=18)
+
+    class Meta:
+        verbose_name = 'currency exchange rate'
+        verbose_name_plural = 'currency exchange rates'
+        ordering = ('valuation_date', 'source_currency')
