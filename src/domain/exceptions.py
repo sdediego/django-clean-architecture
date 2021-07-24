a9a007b8450f0e716e113dc0b00657df08298d32
sdediego <sergio.dediego@outlook.com>
2021-07-24 19:09:34 +0200

    Create entity does not exist exception

--- src/domain/exceptions.py
+++ src/domain/exceptions.py
@@ -5,3 +5,7 @@ class EntityError(Exception):
     def __init__(self, message: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.message = message
+
+
+class EntityDoesNotExist(EntityError):
+    pass
