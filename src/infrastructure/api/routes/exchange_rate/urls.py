688741c295cd1f955093c918848f45f48c3fd357
sdediego <sergio.dediego@outlook.com>
2021-07-27 18:45:21 +0200

    Create currency exchange rate urls

--- src/infrastructure/api/routes/exchange_rate/urls.py
+++ src/infrastructure/api/routes/exchange_rate/urls.py
@@ -3,14 +3,22 @@
 from django.conf.urls import include
 from django.urls import path
 
-from src.infrastructure.api.routes.exchange_rate.routers import CurrencyRouter
-from src.infrastructure.api.views.exchange_rate import CurrencyViewset
+from src.infrastructure.api.routes.exchange_rate.routers import (
+    CurrencyRouter, CurrencyExchangeRateRouter)
+from src.infrastructure.api.views.exchange_rate import (
+    CurrencyViewSet, CurrencyExchangeRateViewSet)
 
 
 currency_router = CurrencyRouter()
-currency_router.register('', viewset=CurrencyViewset, basename='currencies')
+currency_router.register('', viewset=CurrencyViewSet, basename='currencies')
+
+
+exchange_rate_router = CurrencyExchangeRateRouter()
+exchange_rate_router.register(
+    '', viewset=CurrencyExchangeRateViewSet, basename='exchange-rate')
 
 
 urlpatterns = [
+    path('', include(exchange_rate_router.urls)),
     path('currencies/', include(currency_router.urls))
 ]
