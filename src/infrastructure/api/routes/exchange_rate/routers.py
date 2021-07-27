2329aa9a40aa1c081563df76eee1c78871f412ee
sdediego <sergio.dediego@outlook.com>
2021-07-27 18:44:26 +0200

    Create currency exchange rate router

--- src/infrastructure/api/routes/exchange_rate/routers.py
+++ src/infrastructure/api/routes/exchange_rate/routers.py
@@ -2,7 +2,8 @@
 
 from rest_framework.routers import SimpleRouter, Route
 
-from src.infrastructure.factories.exchange_rates import CurrencyViewsetFactory
+from src.infrastructure.factories.exchange_rates import (
+    CurrencyViewSetFactory, CurrencyExchangeRateViewSetFactory)
 
 
 class CurrencyRouter(SimpleRouter):
@@ -11,14 +12,40 @@ class CurrencyRouter(SimpleRouter):
             url=r'^$',
             mapping={'get': 'list'},
             name='{basename}-list',
-            initkwargs={'view_factory': CurrencyViewsetFactory},
+            initkwargs={'viewset_factory': CurrencyViewSetFactory},
             detail=False
         ),
         Route(
             url=r'^(?P<code>\w+)/$',
             mapping={'get': 'get'},
             name='{basename}-get',
-            initkwargs={'view_factory': CurrencyViewsetFactory},
+            initkwargs={'viewset_factory': CurrencyViewSetFactory},
             detail=True
+        )
+    ]
+
+
+class CurrencyExchangeRateRouter(SimpleRouter):
+    routes = [
+        Route(
+            url=r'^$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            initkwargs={'viewset_factory': CurrencyExchangeRateViewSetFactory},
+            detail=False
+        ),
+        Route(
+            url=r'^convert/$',
+            mapping={'get': 'convert'},
+            name='{basename}-convert',
+            initkwargs={'viewset_factory': CurrencyExchangeRateViewSetFactory},
+            detail=False
+        ),
+        Route(
+            url=r'^time-weighted/$',
+            mapping={'get': 'calculate_twr'},
+            name='{basename}-calculate-twr',
+            initkwargs={'viewset_factory': CurrencyExchangeRateViewSetFactory},
+            detail=False
         )
     ]
