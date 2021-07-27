a518150c3f15f19ed704716e49ce07e3507f7cae
sdediego <sergio.dediego@outlook.com>
2021-07-27 17:40:43 +0200

    Create currency exchange rate api views

--- src/infrastructure/api/views/exchange_rate.py
+++ src/infrastructure/api/views/exchange_rate.py
@@ -5,13 +5,32 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 
-class CurrencyViewset(GenericViewSet):
-    view_factory = None
+class CurrencyViewSet(GenericViewSet):
+    viewset_factory = None
 
     def get(self, request: Request, code: str, *args, **kwargs) -> Response:
-        body, status = self.view_factory.create().get(code)
-        return Response(data=body, status=status)
+        payload, status = self.viewset_factory.create().get(code)
+        return Response(data=payload, status=status)
 
     def list(self, request: Request, *args, **kwargs) -> Response:
-        body, status = self.view_factory.create().list()
-        return Response(data=body, status=status)
+        payload, status = self.viewset_factory.create().list()
+        return Response(data=payload, status=status)
+
+
+class CurrencyExchangeRateViewSet(GenericViewSet):
+    viewset_factory = None
+
+    def convert(self, request: Request, *args, **kwargs) -> Response:
+        query_params = request.query_params
+        payload, status = self.viewset_factory.create().convert(query_params)
+        return Response(data=payload, status=status)
+
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        query_params = request.query_params
+        payload, status = self.viewset_factory.create().list(query_params)
+        return Response(data=payload, status=status)
+
+    def calculate_twr(self, request: Request, *args, **kwargs) -> Response:
+        query_params = request.query_params
+        payload, status = self.viewset_factory.create().calculate_twr(query_params)
+        return Response(data=payload, status=status)
