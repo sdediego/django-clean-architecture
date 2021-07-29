8412fa9e20d2616f4b10c59da2c9dee4ac5cc5fb
sdediego <sergio.dediego@outlook.com>
2021-07-29 18:06:01 +0200

    Improve typing and code format for interactors

--- src/usecases/exchange_rate.py
+++ src/usecases/exchange_rate.py
@@ -4,8 +4,6 @@ from typing import List
 
 import pandas as pd
 
-from django.db.models.query import QuerySet
-
 from src.domain.exchange_rate import CurrencyEntity, CurrencyExchangeRateEntity
 
 
@@ -17,7 +15,7 @@ class CurrencyInteractor:
     def get(self, code: str) -> CurrencyEntity:
         return self.currency_repo.get(code)
 
-    def get_availables(self) -> QuerySet[CurrencyEntity]:
+    def get_availables(self) -> List[CurrencyEntity]:
         return self.currency_repo.get_availables()
 
 
@@ -31,21 +29,21 @@ class CurrencyExchangeRateInteractor:
         return self.exchange_rate_repo.get(
             source_currency, exchanged_currency, valuation_date)
 
-    def get_latest(self, source_currency: str, exchanged_currency: str) \
-            -> CurrencyExchangeRateEntity:
+    def get_latest(self, source_currency: str,
+                   exchanged_currency: str) -> CurrencyExchangeRateEntity:
         today = pd.Timestamp.today().strftime('%Y-%m-%d')
         return self.get(source_currency, exchanged_currency, today)
 
     def get_rate_series(self, source_currency: str, exchanged_currency: str,
-            date_from: str, date_to: str) -> List[float]:
+                        date_from: str, date_to: str) -> List[float]:
         return self.exchange_rate_repo.get_rate_series(
             source_currency, exchanged_currency, date_from, date_to)
 
     def get_time_series(self, source_currency: str, exchanged_currency: str,
-            date_from: str, date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
+                        date_from: str, date_to: str) -> List[CurrencyExchangeRateEntity]:
         return self.exchange_rate_repo.get_time_series(
             source_currency, exchanged_currency, date_from, date_to)
 
     def get_all_time_series(self, source_currency: str, date_from: str,
-            date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
+                            date_to: str) -> List[CurrencyExchangeRateEntity]:
         return self.get_time_series(source_currency, None, date_from, date_to)
