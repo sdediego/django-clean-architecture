4d00bdae37d0a3ed40f6af3fabb892d29deff607
sdediego <sergio.dediego@outlook.com>
2021-08-06 23:23:59 +0200

    Create currency exchange rate cache repository

--- src/infrastructure/orm/cache/exchange_rate/constants.py
+++ src/infrastructure/orm/cache/exchange_rate/constants.py
@@ -1,3 +1,4 @@
 # coding: utf-8
 
 CACHE_AVAILABLE_CURRENCIES_KEY = 'cache_available_currencies_key'
+CACHE_EXCHANGE_RATE_KEY = '{source_currency}_{exchanged_currency}_{valuation_date}'
--- src/infrastructure/orm/cache/exchange_rate/repositories.py
+++ src/infrastructure/orm/cache/exchange_rate/repositories.py
@@ -4,9 +4,9 @@ from typing import List, Union
 
 from django.core.cache import cache
 
-from src.domain.exchange_rate import CurrencyEntity
+from src.domain.exchange_rate import CurrencyEntity, CurrencyExchangeRateEntity
 from src.infrastructure.orm.cache.exchange_rate.constants import (
-    CACHE_AVAILABLE_CURRENCIES_KEY)
+    CACHE_AVAILABLE_CURRENCIES_KEY, CACHE_EXCHANGE_RATE_KEY)
 
 
 class CurrencyCacheRepository:
@@ -22,3 +22,28 @@ class CurrencyCacheRepository:
 
     def save_availables(self, currencies: List[CurrencyEntity]):
         self.save(CACHE_AVAILABLE_CURRENCIES_KEY, currencies)
+
+
+class CurrencyExchangeRateCacheRepository:
+
+    @staticmethod
+    def get_exchange_rate_key(source_currency: str, exchanged_currency: str,
+                              valuation_date: str) -> str:
+        return CACHE_EXCHANGE_RATE_KEY.format(
+            source_currency=source_currency,
+            exchanged_currency=exchanged_currency,
+            valuation_date=valuation_date
+        )
+
+    def get(self, source_currency: str, exchanged_currency: str,
+            valuation_date: str) -> CurrencyExchangeRateEntity:
+        key = self.get_exchange_rate_key(
+            source_currency, exchanged_currency, valuation_date)
+        return cache.get(key)
+
+    def save(self, exchange_rate: CurrencyExchangeRateEntity):
+        key = self.get_exchange_rate_key(
+            exchange_rate.source_currency,
+            exchange_rate.exchanged_currency,
+            exchange_rate.valuation_date)
+        cache.set(key, exchange_rate)
