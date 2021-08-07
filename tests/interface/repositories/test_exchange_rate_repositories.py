9f0ab3fca051ef675b21c6d0ce4af3632d628bec
sdediego <sergio.dediego@outlook.com>
2021-08-07 18:03:13 +0200

    Update currency interface repositories tests

--- tests/interface/repositories/test_exchange_rate_repositories.py
+++ tests/interface/repositories/test_exchange_rate_repositories.py
@@ -13,27 +13,76 @@ from tests.fixtures import currency, exchange_rate
 
 
 @pytest.mark.unit
-def test_currency_repository_get(currency):
+def test_currency_repository_database_get(currency):
     db_repo = Mock()
     db_repo.get.return_value = currency
-    currency_repo = CurrencyRepository(db_repo)
+    cache_repo = Mock()
+    cache_repo.get.return_value = None
+    cache_repo.save.return_value = None
+    currency_repo = CurrencyRepository(db_repo, cache_repo)
     result = currency_repo.get(currency.code)
+    assert cache_repo.get.called
+    assert cache_repo.save.called
     assert db_repo.get.called
     assert result.code == currency.code
     assert result.name == currency.name
     assert result.symbol == currency.symbol
-    assert CurrencyEntity.to_string(result) == CurrencyEntity.to_string(currency)
+    assert CurrencyEntity.to_string(
+        result) == CurrencyEntity.to_string(currency)
 
 
 @pytest.mark.unit
-def test_currency_repository_get_availables(currency):
+def test_currency_repository_cache_get(currency):
+    db_repo = Mock()
+    db_repo.get.return_value = None
+    cache_repo = Mock()
+    cache_repo.get.return_value = currency
+    cache_repo.save.return_value = None
+    currency_repo = CurrencyRepository(db_repo, cache_repo)
+    result = currency_repo.get(currency.code)
+    assert cache_repo.get.called
+    assert not cache_repo.save.called
+    assert not db_repo.get.called
+    assert result.code == currency.code
+    assert result.name == currency.name
+    assert result.symbol == currency.symbol
+    assert CurrencyEntity.to_string(
+        result) == CurrencyEntity.to_string(currency)
+
+
+@pytest.mark.unit
+def test_currency_repository_database_get_availables(currency):
     num_of_currencies = random.randint(1, 10)
     currencies_available = [currency for _ in range(num_of_currencies)]
     db_repo = Mock()
     db_repo.get_availables.return_value = currencies_available
-    currency_repo = CurrencyRepository(db_repo)
+    cache_repo = Mock()
+    cache_repo.get_availables.return_value = None
+    cache_repo.save_availables.return_value = None
+    currency_repo = CurrencyRepository(db_repo, cache_repo)
     result = currency_repo.get_availables()
     assert db_repo.get_availables.called
+    assert cache_repo.get_availables.called
+    assert cache_repo.save_availables.called
+    assert isinstance(result, list)
+    assert len(result) == num_of_currencies
+    assert all([isinstance(currency, CurrencyEntity) for currency in result])
+
+
+@pytest.mark.unit
+def test_currency_repository_cache_get_availables(currency):
+    num_of_currencies = random.randint(1, 10)
+    currencies_available = [currency for _ in range(num_of_currencies)]
+    db_repo = Mock()
+    db_repo.get_availables.return_value = None
+    cache_repo = Mock()
+    cache_repo.get_availables.return_value = currencies_available
+    cache_repo.save_availables.return_value = None
+    currency_repo = CurrencyRepository(db_repo, cache_repo)
+    result = currency_repo.get_availables()
+    assert cache_repo.get_availables.called
+    assert not cache_repo.save_availables.called
+    assert not db_repo.get_availables.called
     assert isinstance(result, list)
     assert len(result) == num_of_currencies
     assert all([isinstance(currency, CurrencyEntity) for currency in result])
