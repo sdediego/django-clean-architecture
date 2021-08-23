48da337cc3b997064276b230e5f913e7839e8046
sdediego <sergio.dediego@outlook.com>
2021-08-23 20:56:01 +0200

    Update provider repository for cache

--- src/interface/repositories/provider.py
+++ src/interface/repositories/provider.py
@@ -7,8 +7,13 @@ from src.domain.provider import ProviderEntity
 
 class ProviderRepository:
 
-    def __init__(self, db_repo: object):
+    def __init__(self, db_repo: object, cache_repo: object):
         self.db_repo = db_repo
+        self.cache_repo = cache_repo
 
     def get_by_priority(self) -> List[ProviderEntity]:
-        return self.db_repo.get_by_priority()
+        providers = self.cache_repo.get_by_priority()
+        if not providers:
+            providers = self.db_repo.get_by_priority()
+            self.cache_repo.save(providers)
+        return providers
