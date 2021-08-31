a6dcc52cfa7a0bea86f2427be67257be6a1cb8d0
sdediego <sergio.dediego@outlook.com>
2021-08-31 16:47:38 +0200

    Update provider client interface and database repo

--- src/infrastructure/orm/db/provider/repositories.py
+++ src/infrastructure/orm/db/provider/repositories.py
@@ -2,6 +2,8 @@
 
 from typing import List
 
+from django.db.models import Prefetch
+
 from src.domain.provider import ProviderEntity, ProviderSettingEntity
 from src.infrastructure.orm.db.provider.models import Provider, ProviderSetting
 
@@ -9,15 +11,16 @@ from src.infrastructure.orm.db.provider.models import Provider, ProviderSetting
 class ProviderDatabaseRepository:
 
     def get_by_priority(self) -> List[ProviderEntity]:
+        prefetch = Prefetch('settings', queryset=ProviderSetting.objects.all())
+        providers = Provider.objects.prefetch_related(prefetch).filter(enabled=True)
         return [
             ProviderEntity(
-                name=provider.get('name'),
-                slug=provider.get('slug'),
-                priority=provider.get('priority'),
+                name=provider.name,
+                slug=provider.slug,
+                priority=provider.priority,
                 settings={
                     setting.get('key'): ProviderSettingEntity(**setting)
-                    for setting in ProviderSetting.objects.filter(
-                        provider=provider.get('id')).values('setting_type', 'key', 'value')
+                    for setting in provider.settings.values('setting_type', 'key', 'value')
                 }
-            ) for provider in Provider.objects.filter(enabled=True).values()
+            ) for provider in providers
         ]
--- src/interface/clients/provider.py
+++ src/interface/clients/provider.py
@@ -9,5 +9,4 @@ class ProviderClient:
         self.provider_driver = provider_driver
 
     def get_exchange_rate_data(self, action: str, **kwargs: dict) -> Any:
-        return self.provider_driver.get_exchange_rate_data(
-            action, **kwargs)
+        return self.provider_driver.get_exchange_rate_data(action, **kwargs)
