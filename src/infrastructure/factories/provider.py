937acd9c0abe676d15956f3fc4218674a5bcf3fb
sdediego <sergio.dediego@outlook.com>
2021-09-10 23:29:45 +0200

    Create provider interactor factory

--- src/infrastructure/factories/provider.py
+++ src/infrastructure/factories/provider.py
@@ -3,6 +3,7 @@
 from src.infrastructure.orm.cache.provider.repositores import ProviderCacheRepository
 from src.infrastructure.orm.db.provider.repositories import ProviderDatabaseRepository
 from src.interface.repositories.provider import ProviderRepository
+from src.usecases.provider import ProviderInteractor
 
 
 class ProviderDatabaseRepositoryFactory:
@@ -26,3 +27,11 @@ class ProviderRepositoryFactory:
         db_repo = ProviderDatabaseRepositoryFactory.get()
         cache_repo = ProviderCacheRepositoryFactory.get()
         return ProviderRepository(db_repo, cache_repo)
+
+
+class ProviderInteractorFactory:
+
+    @staticmethod
+    def get():
+        provider_repo = ProviderRepositoryFactory.get()
+        return ProviderInteractor(provider_repo)
