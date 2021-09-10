f017da72d5c5d37908da5b8175a07fa20b8088fd
sdediego <sergio.dediego@outlook.com>
2021-09-10 23:31:45 +0200

    Create provider clients factories

--- src/infrastructure/factories/provider.py
+++ src/infrastructure/factories/provider.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
+from src.infrastructure.clients.provider.drivers import ProviderMasterDriver
 from src.infrastructure.orm.cache.provider.repositores import ProviderCacheRepository
 from src.infrastructure.orm.db.provider.repositories import ProviderDatabaseRepository
+from src.interface.clients.provider import ProviderClient
 from src.interface.repositories.provider import ProviderRepository
 from src.usecases.provider import ProviderInteractor
 
@@ -35,3 +37,19 @@ class ProviderInteractorFactory:
     def get():
         provider_repo = ProviderRepositoryFactory.get()
         return ProviderInteractor(provider_repo)
+
+
+class ProviderDriverFactory:
+
+    @staticmethod
+    def get():
+        provider_interactor = ProviderInteractorFactory.get()
+        return ProviderMasterDriver(provider_interactor)
+
+
+class ProviderClientFactory:
+
+    @staticmethod
+    def get():
+        provider_driver = ProviderDriverFactory.get()
+        return ProviderClient(provider_driver)
