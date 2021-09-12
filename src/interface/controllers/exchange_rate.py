7b8b0adf870c3479eb7dd852cac94cd3d6ea4b8a
sdediego <sergio.dediego@outlook.com>
2021-09-12 17:36:13 +0200

    Add provider client interactor to currency controller

--- src/interface/controllers/exchange_rate.py
+++ src/interface/controllers/exchange_rate.py
@@ -4,7 +4,8 @@ from http import HTTPStatus
 from typing import Tuple
 
 from src.domain.exchange_rate import CurrencyExchangeAmountEntity, TimeWeightedRateEntity
-from src.interface.controllers.utils import calculate_time_weighted_rate
+from src.interface.controllers.utils import (
+    calculate_time_weighted_rate, filter_currencies)
 from src.interface.repositories.exceptions import EntityDoesNotExist
 from src.interface.serializers.exchange_rate import (
     CurrencySerializer, CurrencyExchangeRateAmountSerializer,
@@ -12,22 +13,33 @@ from src.interface.serializers.exchange_rate import (
     CurrencyExchangeRateSerializer, TimeWeightedRateListSerializer,
     TimeWeightedRateSerializer)
 from src.usecases.exchange_rate import CurrencyInteractor, CurrencyExchangeRateInteractor
+from src.usecases.provider import ProviderClientInteractor
 
 
 class CurrencyController:
 
-    def __init__(self, currency_interator: CurrencyInteractor):
+    def __init__(self, currency_interator: CurrencyInteractor,
+                 provider_client_interactor: ProviderClientInteractor):
         self.currency_interator = currency_interator
+        self.provider_client_interactor = provider_client_interactor
 
     def get(self, code: str) -> Tuple[dict, int]:
         try:
             currency = self.currency_interator.get(code.upper())
         except EntityDoesNotExist as err:
-            return {'error': err.message}, HTTPStatus.NOT_FOUND.value
+            currency = filter_currencies(
+                code.upper(),
+                self.provider_client_interactor.fetch_data('currency_get'))
+            if currency is None:
+                return {'error': err.message}, HTTPStatus.NOT_FOUND.value
         return CurrencySerializer().dump(currency), HTTPStatus.OK.value
 
     def list(self) -> Tuple[list, int]:
         currencies = self.currency_interator.get_availables()
+        if not currencies:
+            currencies = self.provider_client_interactor.fetch_data('currency_list')
+            if 'error' in currencies:
+                currencies = []
         return (
             CurrencySerializer(many=True).dump(currencies),
             HTTPStatus.OK.value
--- src/interface/controllers/utils.py
+++ src/interface/controllers/utils.py
@@ -3,6 +3,14 @@
 import operator
 from functools import reduce
 
+from src.domain.exchange_rate import CurrencyEntity
+
 
 def calculate_time_weighted_rate(rate_series: list) -> float:
     return reduce(operator.mul, rate_series)**(1.0 / len(rate_series))
+
+
+def filter_currencies(code: str, currencies: list) -> CurrencyEntity:
+    currency = list(filter(
+        lambda x: x.code == code if hasattr(x, 'code') else False, currencies))
+    return currency[0] if len(currency) > 0 else None
