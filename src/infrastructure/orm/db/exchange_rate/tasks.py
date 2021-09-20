c095206b18c6f756c177359dacd4a31c9ab6ce39
sdediego <sergio.dediego@outlook.com>
2021-09-20 14:00:10 +0200

    Create exchange rate save and bulk save celery tasks

---
+++ src/infrastructure/orm/db/exchange_rate/tasks.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+
+import json
+
+from celery import shared_task
+
+from src.infrastructure.orm.db.exchange_rate.models import (
+    Currency, CurrencyExchangeRate)
+
+
+def get_currency(code: str, name: str = None) -> Currency:
+    try:
+        currency = Currency.objects.get(code=code)
+    except Currency.DoesNotExist:
+        name = name or code
+        currency = Currency.objects.create(code=code, name=name)
+    return currency
+
+
+@shared_task
+def save_currency(currency_json: str):
+    currency = json.loads(currency_json)
+    Currency.objects.create(
+        code=currency.get('code'),
+        name=currency.get('name'),
+        symbol=currency.get('symbol')
+    )
+
+
+@shared_task
+def save_exchange_rate(exchange_rate_json: str):
+    exchange_rate = json.loads(exchange_rate_json)
+    source_currency = get_currency(exchange_rate.get('source_currency'))
+    exchanged_currency = get_currency(exchange_rate.get('exchanged_currency'))
+
+    CurrencyExchangeRate.objects.create(
+        source_currency=source_currency,
+        exchanged_currency=exchanged_currency,
+        valuation_date=exchange_rate.get('valuation_date'),
+        rate_value=exchange_rate.get('rate_value')
+    )
+
+
+@shared_task
+def bulk_save_currencies(currencies_json: str):
+    currencies = json.loads(currencies_json)
+    batch = [
+        Currency(
+            code=currency.get('code'),
+            name=currency.get('name'),
+            symbol=currency.get('symbol')
+        )
+        for currency in currencies
+    ]
+    Currency.objects.bulk_create(batch, ignore_conflicts=True)
+
+
+@shared_task
+def bulk_save_exchange_rates(exchange_rates_json: str):
+    exchange_rates = json.loads(exchange_rates_json)
+    source_currency = get_currency(exchange_rates[0].get('source_currency'))
+    exchanged_codes = set(list(
+        map(lambda x: x.get('exchanged_currency'), exchange_rates)))
+    exchanged_currencies = {
+        code: get_currency(code) for code in list(exchanged_codes)
+    }
+
+    batch = [
+        CurrencyExchangeRate(
+            source_currency=source_currency,
+            exchanged_currency=exchanged_currencies.get(
+                exchange_rate.get('exchanged_currency')),
+            valuation_date=exchange_rate.get('valuation_date'),
+            rate_value=exchange_rate.get('rate_value')
+        )
+        for exchange_rate in exchange_rates
+    ]
+    CurrencyExchangeRate.objects.bulk_create(batch, ignore_conflicts=True)
